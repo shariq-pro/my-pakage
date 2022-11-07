69a610dd3504d7a93ca55504408458095b667957
shariq-pro <shariq.ali@bitswits.com>
2022-11-07 11:58:02 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeadComp from './MyTest/HeadComp';
+
+function App() {
+  return (
+    <div className="App">
+      <HeadComp/>
+    </div>
+  );
+}
+
+export default App;
